70b752ebace06197ca9c6c7cffd5ed084bd4dea8
Sizwe Maluleke <sizwes.maluleke@gmail.com>
2022-11-05 15:21:30 +0200

    added binary target obscuring framework logic

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "DesertBuildingFramework",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +22,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "DesertBuildingFramework",
-            dependencies: []),
-        .testTarget(
-            name: "DesertBuildingFrameworkTests",
-            dependencies: ["DesertBuildingFramework"]),
+        .binaryTarget(name: "DesertBuildingFramework",
+                      path: "./Sources/DesertBuildingFramework.xcframework")
     ]
 )
---
+++ Sources/DesertBuildingFramework.xcframework/ios-arm64_x86_64-simulator/DesertBuildingFramework.framework/Headers/DesertBuildingFramework.h
@@ -0,0 +1,18 @@
+//
+//  DesertBuildingFramework.h
+//  DesertBuildingFramework
+//
+//  Created by Sizwe Maluleke on 2022/11/05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DesertBuildingFramework.
+FOUNDATION_EXPORT double DesertBuildingFrameworkVersionNumber;
+
+//! Project version string for DesertBuildingFramework.
+FOUNDATION_EXPORT const unsigned char DesertBuildingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesertBuildingFramework/PublicHeader.h>
+
+
