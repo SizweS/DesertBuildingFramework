bfdcf31d1597347ec2a38679342fb59ff8db55fc
Sizwe Maluleke <sizwes.maluleke@gmail.com>
2022-11-05 12:03:08 +0200

    first commit

--- Sources/DesertBuildingFramework/DesertBuildingFramework.swift
+++
@@ -1,6 +0,0 @@
-public struct DesertBuildingFramework {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Sources/DesertBuildingFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  DesertBuilding
+//
+//  Created by Sizwe Maluleke on 2022/11/05.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomething() {
+        print("Something...")
+    }
+}
